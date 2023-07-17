21150fef683504fa99c938e31f7361ad7819a7d5
Iryna <ira.shkraba@ukr.net>
2023-07-17 18:51:55 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const IngredientsUl = document.getElementById('ingredients');
+ingredients.forEach(ingredient => {
+const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+IngredientsUl.appendChild(liEl);
+});
+document.body.appendChild(IngredientsUl);
+
+
