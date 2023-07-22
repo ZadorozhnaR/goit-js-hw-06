1ac0dfacd2cc25930d45f7b299615302baf501f6
Iryna <ira.shkraba@ukr.net>
2023-07-22 12:04:34 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+const message = "Всі поля повинні бути заповнені!";
 
+form.addEventListener('submit', functionSubmit);
+
+
+function functionSubmit(e) {
+    e.preventDefault();
+    const { elements: { email, password } } = e.currentTarget;
+    if (email.value.trim() === "" || password.value.trim() === "") {
+        return alert(message);
+    }
+    else {
+        const formData = { email:email.value, password:password.value };
+        console.log(formData);
+        e.currentTarget.reset();
+    };
+}
