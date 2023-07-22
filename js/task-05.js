d71402b2fad1d5a5307b0dde8a4e4769932adcdd
Iryna <ira.shkraba@ukr.net>
2023-07-22 22:01:31 +0300

    Create task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const nameInp = document.getElementById('name-input');
+const nameOut = document.getElementById('name-output');
+
+
+nameInp.addEventListener("input", newName);
+
+function newName({ target }) {
+    if(target.value.trim() !==""){
+        nameOut.textContent = target.value;
+    }
+    else {
+        nameOut.textContent = "Anonymous";
+    }
+}
