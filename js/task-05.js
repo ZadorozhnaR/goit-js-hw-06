605729974fdc402dfba8dd5ce2f01f1d4bcd53e2
Iryna <ira.shkraba@ukr.net>
2023-07-24 09:33:01 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,14 +1,12 @@
-const nameInp = document.getElementById('name-input');
-const nameOut = document.getElementById('name-output');
-
+const nameInp = document.getElementById("name-input");
+const nameOut = document.getElementById("name-output");
 
 nameInp.addEventListener("input", newName);
 
 function newName({ target }) {
-    if(target.value.trim() !==""){
-        nameOut.textContent = target.value;
-    }
-    else {
-        nameOut.textContent = "Anonymous";
-    }
+  if (target.value.trim() !== "") {
+    nameOut.textContent = target.value;
+  } else {
+    nameOut.textContent = "Anonymous";
+  }
 }
