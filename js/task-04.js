01321104ab9b6cab8c98049a40a26466de1c57d2
Iryna <ira.shkraba@ukr.net>
2023-07-24 09:32:58 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,18 @@
-const counterValue = document.getElementById('value');
+const counterValue = document.getElementById("value");
 const buttonDec = document.querySelector('div button[data-action="decrement"]');
-const buttonDInc = document.querySelector('div button[data-action="increment"]');
+const buttonDInc = document.querySelector(
+  'div button[data-action="increment"]'
+);
 buttonDec.addEventListener("click", actionMin);
 buttonDInc.addEventListener("click", actionPlus);
 let valueStart = 0;
 
 function actionMin(event) {
-    valueStart -= 1;
-    counterValue.textContent = valueStart;   
+  valueStart -= 1;
+  counterValue.textContent = valueStart;
 }
 
 function actionPlus(event) {
-    valueStart += 1;
-    counterValue.textContent = valueStart;
+  valueStart += 1;
+  counterValue.textContent = valueStart;
 }
