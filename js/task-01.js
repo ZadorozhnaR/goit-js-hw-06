a66691e6050c4324f1135ad6e478fa8d462ba17c
Iryna <ira.shkraba@ukr.net>
2023-07-24 09:32:50 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,15 @@
-const categoriesUl = document.querySelectorAll('.item');
+const categoriesUl = document.querySelectorAll(".item");
 console.log(`Number of categories : ${categoriesUl.length}`);
 
-const categoriesParent = document.querySelector('#categories');
-const categoriesIt = categoriesParent.querySelectorAll('#categories .item');
+const categoriesParent = document.querySelector("#categories");
+const categoriesIt = categoriesParent.querySelectorAll("#categories .item");
 
 const categoriesList = [];
-categoriesIt.forEach(element => {categoriesList.push(`Category: ${element.querySelector('h2').textContent} Elements: ${element.querySelector('ul').children.length}`) });
-console.log(categoriesList);
+categoriesIt.forEach((element) => {
+  categoriesList.push(
+    `Category: ${element.querySelector("h2").textContent} Elements: ${
+      element.querySelector("ul").children.length
+    }`
+  );
+});
+console.log(categoriesList.join("; "));
