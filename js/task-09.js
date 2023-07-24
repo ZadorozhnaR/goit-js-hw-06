6260e56bee7268f1a696c7340fbb3e56b53257b1
Iryna <ira.shkraba@ukr.net>
2023-07-24 09:33:12 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,8 @@
-
 const body = document.body;
-const btn = document.querySelector('.change-color');
-const colorName = document.querySelector('.color');
+const btn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
 
 btn.addEventListener("click", changeColor);
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -15,4 +13,3 @@ function getRandomHexColor() {
 function changeColor() {
   colorName.textContent = body.style.backgroundColor = getRandomHexColor();
 }
-
